2c371971a948f87823aab02fe6eb301fb2394fdb
Chris hsiao <hsiao516@gmail.com>
2020-09-23 11:05:33 +0800

    [新增] 影片頁播放暫停的時候會跳出廣告功能

--- src/page/Video.js
+++ src/page/Video.js
@@ -7,9 +7,22 @@ class Video extends React.Component {
     constructor(props) {
         super(props);
         this.props = props;
+
+        this.state = {
+            isAdOpen: false
+        }
+
+        this.handleAdContainer = this.handleAdContainer.bind(this);
+    }
+
+    handleAdContainer() {
+        const { isAdOpen } = this.state;
+
+        this.setState({ isAdOpen: !isAdOpen });
     }
 
     render() {
+        const { isAdOpen } = this.state;
         const url = this.props.location.state ?
             `https://www.youtube.com/embed/${this.props.location.state.videoId}`
             :
@@ -23,6 +36,7 @@ class Video extends React.Component {
                     <ReactPlayer
                         url={url}
                         controls={true}
+                        onPause={() => this.handleAdContainer()}
                     />
 
                     {
@@ -32,12 +46,49 @@ class Video extends React.Component {
                         </Content>
                     }
                 </Container>
+
+                {
+                    isAdOpen && <AdContainer>
+                        <div style={{ position: 'relative' }}>
+                            <AdButton
+                                onClick={this.handleAdContainer}
+                            >X</AdButton>
+                            <img src="https://mrmad.com.tw/wp-content/uploads/2016/11/delete-iphone-advertising-messages.png" />
+                        </div>
+                    </AdContainer>
+                }
             </>
         );
     }
 }
 
 export default Video;
+
+const AdContainer = styled.div`
+    position: fixed;
+    left: 0;
+    top: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 100%;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.3);
+`;
+
+const AdButton = styled.div`
+    position: absolute;
+    right: 0;
+    top: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background-color: #FFF;
+    width: 50px;
+    height: 50px;
+    color: #181818;
+    cursor: pointer;
+`;
 
 const Container = styled.div`
     display: flex;
